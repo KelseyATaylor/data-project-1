3f73232c70171c6b140311afef4545abe39d499d
Kelsey Taylor <kelseytay94@gmail.com>
2022-10-13 14:43:56 -0600

    first commit - completed through csv file creation

---
+++ starter/delete-tables.py
@@ -0,0 +1,35 @@
+import psycopg2
+from psycopg2 import Error
+
+from database import get_connected
+
+
+try:
+    
+    connection = get_connected()
+
+    # creates cursor
+    cursor = connection.cursor()
+
+    ############### CREATING TABLES AND INSERTING INFORMATION ###############
+
+    delete_customers = """ DROP TABLE customers; """
+    delete_invoices = """ DROP TABLE invoices; """
+
+    cursor.execute(delete_customers)
+    connection.commit()
+    print("customers deleted")
+
+    cursor.execute(delete_invoices)
+    connection.commit()
+    print("invoices deleted")
+
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL DB", error)
+
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("DB connection is closed.")
